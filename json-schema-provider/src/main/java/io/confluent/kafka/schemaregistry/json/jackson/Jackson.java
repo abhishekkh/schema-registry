182aa27a1da2969bc0c74aa8f2af49af092ceac6
ahodavdekar <ahodavdekar@marqeta.com>
2022-03-31 23:32:14 -0500

    Revert "add blackbird module for serialization"
    This reverts commit 338cbcc016fc70ca01baf0268317c9f5c19bac27.

--- json-schema-provider/src/main/java/io/confluent/kafka/schemaregistry/json/jackson/Jackson.java
+++ json-schema-provider/src/main/java/io/confluent/kafka/schemaregistry/json/jackson/Jackson.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
@@ -65,7 +64,6 @@ public class Jackson {
     mapper.registerModule(new Jdk8Module());
     mapper.registerModule(new JavaTimeModule());
     mapper.registerModule(new JsonOrgModule());
-    mapper.registerModule(new BlackbirdModule());
     mapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
     mapper.disable(FAIL_ON_UNKNOWN_PROPERTIES);
     mapper.setNodeFactory(JsonNodeFactory.withExactBigDecimals(true));
